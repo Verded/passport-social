d1e6b1e761868188443b57cbd84fa334fafaa6fe
Yuinshiel <wee.jiawei.kevan@dhs.sg>
2017-04-12 19:14:55 +0800

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -1,6 +1,6 @@
 // twitter app settings
 module.exports = {
-	'apikey' : '<your_api_key>',
-	'apisecret' : '<your_api_secret>',
+	'apikey' : '7sKQusDXSrk1aKQwjmFSMs4Cd',
+	'apisecret' : '	iAT1yVIkUvJQ6dJgYV2O3cdufK7IjDbjsfrBtevDlhZvImfKGB',
 	'callbackUrl' : 'http://127.0.0.1:3000/login/twitter/callback'
 }
